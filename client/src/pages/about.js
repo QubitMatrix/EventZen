1cb5c3171bd3129000fe6e44a52d277d23c5d369
QubitMatrix <preethi13m@gmail.com>
2022-11-20 22:31:06 +0530

    about page

---
+++ client/src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Info(){
+    return(
+        <div id="about-mid">
+            <h2>WELCOME TO EVENT</h2>
+            <i>Registration made easy!</i><br></br>
+            <span>This website displays all the events happening in your campus. It provides a platform for event planners to advertise their events and an easy registration process for the fun seekers.</span><br/>
+            <span>It provides various facilities from registration, easy search and a review section.It also provides a calender to keep you updated of the events happening around and a contacts page for you to contact the event heads for any queries.</span><br/>
+            <span>Head over to the gallery section to have a view of what EventZen looked like in its previous editions.</span><br/>
+            <br></br>
+            <b>COME JOIN THE FUN</b>
+        </div>
+
+
+    )
+}
